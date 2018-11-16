a8c51b23bc54dfb9433bb06856cc4189308680f4
klalle <kalle.lofgren@gmail.com>
2018-11-16 18:37:36 +0100

    bedsize to 1m

--- Marlin_1_1_5_Customized_LCD_Menu/Configuration.h
+++ Marlin_1_1_5_Customized_LCD_Menu/Configuration.h
@@ -771,8 +771,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 455 //Kalle
-#define Y_BED_SIZE 335 //Kalle
+#define X_BED_SIZE 999 //455 Kalle
+#define Y_BED_SIZE 999 //335 Kalle
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
